cc2bf0e96d5dd3386e24d7fa215ac03e1085e969
JacekHacking <Jacekhacking@mac.com>
2023-02-07 12:24:57 -0700

    tailwind added events file and routes setup

---
+++ pages/events/[...slug].tsx
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h1 className="text-red-900 text-7xl">Hello from filtered Events Page</h1>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].tsx
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>EVENT DETAIL PAGE</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
